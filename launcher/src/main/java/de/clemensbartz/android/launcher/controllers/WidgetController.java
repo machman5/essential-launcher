15013ff262c671310273eed3c1fb84436f0c874e
Clemens Bartz <clemens@clemens-bartz.de>
2019-06-09 15:03:37 +0200

    Optimize imports.

--- launcher/src/main/java/de/clemensbartz/android/launcher/controllers/WidgetController.java
+++ launcher/src/main/java/de/clemensbartz/android/launcher/controllers/WidgetController.java
@@ -23,7 +23,6 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -33,7 +32,6 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
-import android.widget.ViewFlipper;
 
 import de.clemensbartz.android.launcher.Launcher;
 import de.clemensbartz.android.launcher.R;
