3f9948f0c339c981e35a3ba5afc06d978a0904ed
Clemens Bartz <clemens@clemens-bartz.de>
2019-09-23 11:50:24 +0200

    Add Abstract Test test case.

---
+++ launcher/src/androidTest/java/de/clemensbartz/android/launcher/AbstractTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2019  Clemens Bartz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.clemensbartz.android.launcher;
+
+import android.view.View;
+
+import androidx.test.espresso.accessibility.AccessibilityChecks;
+
+import com.google.android.apps.common.testing.accessibility.framework.AccessibilityViewCheckResult;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.BeforeClass;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResultUtils.matchesViews;
+import static de.clemensbartz.androidx.matcher.ViewMatchers.childAtPosition;
+import static org.hamcrest.Matchers.allOf;
+
+/**
+ * Abstract test class. All instrumentation tests have to have this
+ * class as their super class.
+ * @author Clemens Bartz
+ * @since 2.2
+ */
+public abstract class AbstractTest {
+
+    /**
+     * Check accessibility along the way.
+     */
+    @BeforeClass
+    public static void enableAccessibilityChecks() {
+        AccessibilityChecks.enable().setRunChecksFromRootView(true).setSuppressingResultMatcher(getSuppressingResultMatcher());
+    }
+
+    /**
+     * @return any excluded matchers
+     */
+    private static Matcher<? super AccessibilityViewCheckResult> getSuppressingResultMatcher() {
+        //noinspection unchecked
+        return allOf(
+                // Exclude the "More Options" ActionBar overflow button for API 23 and higher
+                matchesViews(
+                        allOf(
+                                withContentDescription("More options"),
+                                childAtPosition(
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
+                                                2),
+                                        1),
+                                isDisplayed()
+                        )
+                )
+        );
+    }
+}
---
+++ launcher/src/androidTest/java/de/clemensbartz/androidx/matcher/ViewMatchers.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2019  Clemens Bartz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.clemensbartz.androidx.matcher;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Provides additional functionality not provided by
+ * {@link androidx.test.espresso.matcher.ViewMatchers ViewMatchers} class
+ * of Espresso.
+ * @since 2.2
+ * @author Clemens Bartz
+ */
+public final class ViewMatchers {
+
+    /**
+     * Hidden constructor.
+     */
+    public ViewMatchers() {
+
+    }
+
+    /**
+     * Returns a matcher for checking a child at a position.
+     * @param parentMatcher the parent matcher
+     * @param position the position
+     * @return a matcher
+     */
+    public static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
